de42a5ebdd7669f4a6896f17148a17958f6a3ee0
Andrew Warner <wwarner.andrew@gmail.com>
2013-10-30 09:41:42 -0400

    Add webhook actions

--- app.rb
+++ app.rb
@@ -11,18 +11,30 @@ class Trello
     @key, @token = key, token
   end
 
-  def get(path, options = {})
-    options[:query] = options.fetch(:query, {}).merge(key: key, token: token)
-    self.class.get(path, options).body
+  ['get', 'post', 'put'].each do |m|
+    define_method(m) { |*args| trello_request(m, *args) }
   end
 
   private
   attr_reader :key, :token
+
+  def trello_request(method, path, options = {})
+    options[:query] = options.fetch(:query, {}).merge(key: key, token: token)
+    self.class.send(method, path, options).body
+  end
 end
 
-# https://trello.com/1/authorize?key=<my_key>&name=trello_mentions&scope=read&expiration=never&response_type=token
 trello = Trello.new(ENV.fetch('TRELLO_KEY'), ENV.fetch('TRELLO_TOKEN'))
 
 get '/' do
   "Hello, world"
+end
+
+head '/webhook' do
+end
+
+post '/webhook' do
+  puts params.inspect
+  parsed_body = Map.new(JSON.parse(request.body.read))
+  puts parsed_body.inspect
 end

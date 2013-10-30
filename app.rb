2e7573ce70ec01b741312a00d1e8cb2ecf0633e4
Andrew Warner <wwarner.andrew@gmail.com>
2013-10-30 09:13:25 -0400

    Add simple trello client

--- app.rb
+++ app.rb
@@ -3,7 +3,25 @@ Bundler.require
 
 Dotenv.load
 
-TRELLO_KEY, TRELLO_SECRET = ENV.fetch('TRELLO_KEY'), ENV.fetch('TRELLO_SECRET')
+class Trello
+  include HTTParty
+  base_uri 'https://api.trello.com/1'
+
+  def initialize(key, token)
+    @key, @token = key, token
+  end
+
+  def get(path, options = {})
+    options[:query] = options.fetch(:query, {}).merge(key: key, token: token)
+    self.class.get(path, options).body
+  end
+
+  private
+  attr_reader :key, :token
+end
+
+# https://trello.com/1/authorize?key=<my_key>&name=trello_mentions&scope=read&expiration=never&response_type=token
+trello = Trello.new(ENV.fetch('TRELLO_KEY'), ENV.fetch('TRELLO_TOKEN'))
 
 get '/' do
   "Hello, world"

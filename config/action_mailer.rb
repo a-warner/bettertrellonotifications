2ba2be453b0c7bd2708fb30a6489154a5384cc1c
Andrew Warner <wwarner.andrew@gmail.com>
2013-10-31 10:13:28 -0400

    Mailgun settings

--- config/action_mailer.rb
+++ config/action_mailer.rb
@@ -3,10 +3,16 @@ ActionMailer::Base.view_paths = File.expand_path "../../views", __FILE__
 ActionMailer::Base.raise_delivery_errors = true
 
 if ENV['RACK_ENV'] == 'production'
-  # something
-  ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = { }
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.smtp_settings = {
+      :authentication => :plain,
+      :address => "smtp.mailgun.org",
+      :port => 587,
+      :domain => ENV.fetch('MAILGUN_DOMAIN'),
+      :user_name => ENV.fetch('MAILGUN_USERNAME'),
+      :password => ENV.fetch('MAILGUN_PASSWORD')
+  }
 else
   ActionMailer::Base.add_delivery_method(:letter_opener, LetterOpener::DeliveryMethod, :location => File.expand_path('../../tmp/letter_opener', __FILE__))
   ActionMailer::Base.delivery_method = :letter_opener

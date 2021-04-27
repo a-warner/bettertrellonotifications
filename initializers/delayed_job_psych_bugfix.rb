39b89aba93ad3de1fb08b559c3528db5fa6168dc
Andrew Warner <wwarner.andrew@gmail.com>
2021-04-27 09:55:52 -0400

    Wip mess around with gems to get console up

--- initializers/delayed_job_psych_bugfix.rb
+++ initializers/delayed_job_psych_bugfix.rb
@@ -1,17 +1,17 @@
-delayed_job_version = Gem.loaded_specs['delayed_job'].version.to_s
+# delayed_job_version = Gem.loaded_specs['delayed_job'].version.to_s
 
-unless '4.0.6' == delayed_job_version
-  raise LoadError, "Delayed Job has been upgraded to #{delayed_job_version}, consider deleting this monkeypatch"
-end
+# unless '4.0.6' == delayed_job_version
+#   raise LoadError, "Delayed Job has been upgraded to #{delayed_job_version}, consider deleting this monkeypatch"
+# end
 
-require 'delayed_job'
+# require 'delayed_job'
 
-[Psych::Visitors::ToRuby, Delayed::PsychExt::ToRuby].each do |clazz|
-  clazz.prepend(Module.new do
-    def visit_Psych_Nodes_Mapping(object)
-      return revive(Psych.load_tags[object.tag].constantize, object) if Psych.load_tags[object.tag]
+# [Psych::Visitors::ToRuby, Delayed::PsychExt::ToRuby].each do |clazz|
+#   clazz.prepend(Module.new do
+#     def visit_Psych_Nodes_Mapping(object)
+#       return revive(Psych.load_tags[object.tag].constantize, object) if Psych.load_tags[object.tag]
 
-      super(object)
-    end
-  end)
-end
+#       super(object)
+#     end
+#   end)
+# end
